2ae8f4db361040f2f3bf01a92f4c26995e274717
Jayden Liaw <liawjianwei@outlook.com>
2024-05-06 23:35:42 +0800

    remove studio and added text
    added inter font

--- src/components/Offices.tsx
+++ src/components/Offices.tsx
@@ -33,9 +33,7 @@ export function Offices({
     <ul role="list" {...props}>
       <li>
         <Office name="Singapore" invert={invert}>
-          93 Lor L Telok Kurau
           <br />
-          Singapore 425597
         </Office>
       </li>
       {/* <li>
